8b9dce1bb75a42e4e7edaa638cf71a2129b702ec
ZzzXCcoder <makaka1337@list.ru>
2025-12-13 22:53:58 +0300

    addedNewScrollAndActiveNav

--- lesson_6/app/src/js/main.ts
+++ lesson_6/app/src/js/main.ts
@@ -1,18 +1,63 @@
-const links : NodeListOf<HTMLAnchorElement> = document.querySelectorAll('a[href^="#"]');
 
-links.forEach((link: HTMLAnchorElement) => {
+const links: HTMLAnchorElement[] = Array.from(document.querySelectorAll('a[href^="#"]'));
+
+
+const sections: HTMLElement[] = links
+    .map(link => document.querySelector<HTMLElement>(link.getAttribute('href')!))
+    .filter((el): el is HTMLElement => el !== null);
+
+
+let sectionOffsets: number[] = sections.map(section => section.offsetTop);
+
+
+window.addEventListener('resize', () => {
+    sectionOffsets = sections.map(section => section.offsetTop);
+});
+
+links.forEach(link => {
     link.addEventListener('click', (event: MouseEvent) => {
+        const href = link.getAttribute('href');
+        if (!href) return;
+        const target = document.querySelector<HTMLElement>(href);
+        if (!target) return;
         event.preventDefault();
-        const targedId: string | null = link.getAttribute('href');
-        if (targedId === null) {
-            return;
-        }
-        const targetSection: HTMLElement | null = document.querySelector(targedId);
-        if (targetSection === null) {
-            return;
-        }
-        targetSection.scrollIntoView({behavior: 'smooth'});
+        target.scrollIntoView({ behavior: 'smooth' });
     });
-})
+});
+
+
+window.addEventListener('scroll', () => {
+    const scrollY = window.scrollY;
+    const activeIndex = getActiveSectionIndex(scrollY);
+
+    if (activeIndex !== -1) {
+       setTimeout(()=>setActiveLink(activeIndex), 20);
+
+    }
+});
+
+
+function getActiveSectionIndex(scrollY: number): number {
+    for (let i = 0; i < sections.length; i++) {
+        const top = sectionOffsets[i];
+        const nextTop = sectionOffsets[i + 1] ?? Infinity;
+        if (scrollY >= top && scrollY < nextTop) return i;
+    }
+    return -1;
+}
+
+
+function setActiveLink(activeIndex: number) {
+    links.forEach((link, i) => {
+        const li = link.parentElement; // <li>
+        if (!li) return;
+
+        if (i === activeIndex) {
+            li.classList.add('active');
+        } else {
+            li.classList.remove('active');
+        }
+    });
+}
 
 
