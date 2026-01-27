6514cbef03a37e4b3dac1536a72dee1f320ad7cb
ZzzXCcoder <makaka1337@list.ru>
2026-01-27 16:26:28 +0300

    chilltime

--- Proj/quiz/src/app/quiz/Dto/quizRequestDto.ts
+++ Proj/quiz/src/app/quiz/Dto/quizRequestDto.ts
@@ -13,21 +13,29 @@ export interface QuizResponse {
 export type QuizItemRequest = RangeItemRequest | SelectItemRequest | TextItemRequest
 
 export interface RangeItemRequest {
+  name: string;
+  description: string;
   min: number;
   max: number;
   type: 'range';
 }
 export interface SelectItemRequest {
+  name: string;
+  description: string;
   options: string[];
   type: 'select';
 }
 export interface TextItemRequest {
+  name: string;
+  description: string;
   placeholder: string;
   type: 'text'
 }
 export type QuizItemResponse = RangeItemResponse | SelectItemResponse | TextItemResponse
 
 export interface RangeItemResponse {
+  name: string;
+  description: string;
   min: number;
   max: number;
   type: 'range';
@@ -35,12 +43,16 @@ export interface RangeItemResponse {
   quizId: number;
 }
 export interface SelectItemResponse {
+  name: string;
+  description: string;
   options: string[];
   type: 'select';
   id: number;
   quizId: number;
 }
 export interface TextItemResponse {
+  name: string;
+  description: string;
   placeholder: string;
   type: 'text'
   id: number;
--- Proj/quiz/src/app/quiz/quiz.component/quiz.component.ts
+++ Proj/quiz/src/app/quiz/quiz.component/quiz.component.ts
@@ -1,6 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import {FormArray, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
-import {QuizItemRequest} from '../Dto/quizRequestDto'
+import {QuizItemRequest, QuizRequest} from '../Dto/quizRequestDto'
+import {QuizService} from '../quizService/quiz.service';
 
 @Component({
   selector: 'app-quiz',
@@ -11,29 +12,94 @@ import {QuizItemRequest} from '../Dto/quizRequestDto'
 })
 
 export class QuizComponent {
+  typeToAdd : 'text' | 'range' | 'select' = 'text';
+   wantAddQuestion : boolean = false;
+   question : FormGroup;
+   constructor(private quizService: QuizService) {
+     this.question = new FormGroup({
+       name : new FormControl('', Validators.required),
+       description : new FormControl('', Validators.required),
+       questions: new FormArray([])
+     });
+   }
+   private getQuestion() {
+     return this.question.get('questions') as FormArray ;
+   }
+   setAddQuestionButtonFalse(){
+     this.wantAddQuestion = true;
+   }
+   addQuestion(type:  'text' | 'range' | 'select') : void {
+     let newQuestion : FormGroup;
+     if (type === 'text') {
+       newQuestion = new FormGroup({
+         type: new FormControl('text'),
+         title: new FormControl('', Validators.required),
+         description: new FormControl('', Validators.required),
+       })
+     }
+     else if (type === 'range') {
+       newQuestion = new FormGroup({
+         type: new FormControl('range'),
+         title: new FormControl('', Validators.required),
+         description: new FormControl('', Validators.required),
+         min: new FormControl('', Validators.required),
+         max : new FormControl('', Validators.required),
+       })
+     }
+     else if (type === 'select') {
+       newQuestion = new FormGroup({
+         type: new FormControl('select'),
+         title: new FormControl('', Validators.required),
+         description: new FormControl('', Validators.required),
+         options : new FormArray([])
+       })
+     }
+     else {
+       newQuestion = new FormGroup({})
+       return;
+     }
+     this.wantAddQuestion = false;
+      this.getQuestion().push(newQuestion);
+   }
+   removeQuestion(index : number) {
+     this.getQuestion().removeAt(index);
+   }
+   submit(){
+     let quizDto = this.convertFromFormToDto()
+     if (!quizDto) {
+       return;
+     }
+     else{
+       this.quizService.postQuizes(quizDto).subscribe({
+         next: (res) => {
+           this.question.reset();
+         },
+         error: (err) => {
+           console.log(err);
+         }
+       })
+     }
+   }
 
-  createQuizForm: FormGroup;
-  constructor() {
-    const quizType:  'range' | 'select' | 'text' = 'text';
-    this.createQuizForm = new FormGroup({
-      name: new FormControl('', [Validators.required]),
-      description: new FormControl('', [Validators.required]),
-      items: new FormArray<FormGroup>([]),
-    })
-  }
-  selectType(){
+   private convertFromFormToDto() : QuizRequest | null {
 
-  }
-  buildFormforType(quizType:  'range' | 'select' | 'text', item : FormGroup) : void {
-      Object.keys(item).forEach(key => {
-        item.removeControl(key);
-      })
-    if (quizType === 'range') {
-      item.addControl('min', new FormControl(0, Validators.required));
-      item.addControl('max', new FormControl(0, Validators.required));
-    }
-    if (quizType === 'select') {
-      
-    }
-  }
+     let questionFormValue = this.question.value;
+     let questuinsItemRequest: QuizItemRequest[] = questionFormValue.questions.map((q :any) => {
+       if (q.type === 'text') {
+         return {title: q.title, description: q.description, placeholder: q.placeholder};
+       }
+       else if (q.type === 'range') {
+         return {title: q.title, description: q.description, min: q.min, max: q.max};
+       }
+       else if (q.type === 'select') {
+         return {title: q.title, description: q.description, options: q.options };
+       }
+       else{
+         return {title: q.title, description: q.description};
+       }
+     })
+     return {name: questionFormValue.name, description: questionFormValue.description, items: questuinsItemRequest}
+
+   }
+
 }
