e94c391309027b6982ca0217efe5a75fe3de1aa2
ZzzXCcoder <makaka1337@list.ru>
2026-02-08 19:54:20 +0300

    prime

--- Proj/quiz/src/app/quiz/showquizes.component/showquizes.component.ts
+++ Proj/quiz/src/app/quiz/showquizes.component/showquizes.component.ts
@@ -1,26 +1,68 @@
-import {ChangeDetectorRef, Component} from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {ShowQuizService} from '../quizService/show-quiz.service';
 import {ShowQuizDto} from '../Dto/show-quizRequestDto';
-import {RangeItemResponse, ShowQuizResponse} from '../Dto/show-quizResponceDto';
+import {ShowQuizResponse} from '../Dto/show-quizResponceDto';
+import {FormsModule} from '@angular/forms';
 
 
 @Component({
   selector: 'app-showquizes',
   templateUrl: './showquizes.component.html',
+  standalone : true,
+  imports: [
+    FormsModule,
+  ],
   styleUrls: ['./showquizes.component.scss']
 })
-export class ShowquizesComponent {
+export class ShowquizesComponent implements OnInit{
   quizes: ShowQuizResponse['items'] = [];
+  quizesPage?: ShowQuizResponse;
+  size : number = 5;
+  dto: ShowQuizDto = { pageNumber: 1, pageSize: 5 };
 
-  constructor(private showQuizService: ShowQuizService, private cdr: ChangeDetectorRef) {}
+  constructor(private showQuizService: ShowQuizService, private cdr: ChangeDetectorRef) {
+  }
 
-  ngOnInit() {
-    const dto: ShowQuizDto = { pageNumber: 1, pageSize: 5 };
-    this.showQuizService.showQuizes(dto).subscribe(res => {
+  ngOnInit(): void {
+    debugger
+    this.loadQuizes();
+    this.loadQuizes();
+  }
+  loadQuizes() {
+     this.showQuizService.showQuizes(this.dto).subscribe(res => {
+      this.quizesPage = res;
       this.quizes = res.items;
       console.log(this.quizes);
-      this.cdr.detectChanges();
     });
+    this.cdr.detectChanges();
+  }
+
+  nextPageNumber() {
+    debugger
+    if (!this.quizesPage) return;
+
+    if (!this.quizesPage.hasNextPage) {
+      return;
+    }
+
+    this.dto.pageNumber++;
+    this.loadQuizes();
+  }
+
+  prevPageNumber() {
+    debugger
+    if (!this.quizesPage) return;
+
+    if (!this.quizesPage.hasPreviousPage) {
+      return;
+    }
+    this.dto.pageNumber--;
+    this.loadQuizes();
+  }
+  changePageSize() {
+    debugger
+    this.dto.pageSize = this.size;
+    this.loadQuizes();
   }
 }
 
